42e3fb173a1f8bae1196064a84fa69866b3985c2
manhtruongbui63 <manhtruongbui63@gmail.com>
2024-08-09 11:23:51 +0700

    cfg-dockerfile

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,4 @@
 import { env } from '@/configs/evironment'
 
-export const WHITELIST_DOMAINS = [
-  'http://localhost:8080',
-  env.FRONT_END_URL,
-  'http://192.168.1.84:8080',
-  'http://192.168.1.83:8080'
-]
+export const WHITELIST_DOMAINS = [env.FRONT_END_URL]
 export const LIST_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
